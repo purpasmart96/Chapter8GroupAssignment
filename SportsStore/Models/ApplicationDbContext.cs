ca0b62a00fad6670f71552625daa31b19bf25958
purpasmart96 <purpasmart@gmail.com>
2021-01-31 16:53:38 -0800

    Initial commit

---
+++ SportsStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportsStore.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using SportsStore.Models;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
