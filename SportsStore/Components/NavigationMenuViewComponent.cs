796a87ac700b381325d3061dbcbd6417275fe2c1
purpasmart96 <purpasmart@gmail.com>
2021-02-09 13:25:10 -0800

    Figure 9-2

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello from the Nav View Component";
+        }
+    }
+}
