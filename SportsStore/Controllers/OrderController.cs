f02dce2711c798a5c39001d196b5bb2a4be4d73d
purpasmart96 <purpasmart@gmail.com>
2021-03-02 18:29:21 -0800

    Adding changes from figure 10-4

---
+++ SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout() => View(new Order());
+    }
+}
