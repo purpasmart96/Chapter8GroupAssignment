2ac296ffd7e74a3de16259badce312b1d49130b2
purpasmart96 <purpasmart@gmail.com>
2021-03-16 20:36:32 -0700

    Adding changes from figure 11-4

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
@@ -13,5 +14,8 @@ namespace SportsStore.Controllers
         }
 
         public ViewResult Index() => View(repository.Products);
+
+        public ViewResult Edit(int productId) => View(repository.Products.FirstOrDefault(p => p.ProductID == productId));
+
     }
 }
