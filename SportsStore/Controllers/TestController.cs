98d117ec578e02b0eaf885a3b107612d891aaf37
purpasmart96 <purpasmart@gmail.com>
2021-05-18 16:10:14 -0700

    Week 6 Assignment

---
+++ SportsStore/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SportsStore.Controllers
+{
+    public class TestController : Controller
+    {
+        public JsonResult Index()
+        {
+            //var data = System.IO.File.ReadAllText("people.json");
+            var new_data = JsonConvert.DeserializeObject(System.IO.File.ReadAllText("people.json"));
+            return Json(new_data);
+        }
+    }
+}
